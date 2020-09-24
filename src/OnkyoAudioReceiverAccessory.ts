a6e5139fe1b21ec63cacb175898bdeb5798dcc1b
Tobias Timpe <tobias.timpe@hunter.de>
2020-09-24 10:07:16 +0200

    removed enabledServices

--- src/OnkyoAudioReceiverAccessory.ts
+++ src/OnkyoAudioReceiverAccessory.ts
@@ -184,10 +184,6 @@ export default class OnkyoAudioReceiverAccessory {
 		this.enabledServices.push(this.tvSpeakerService)	
 	}
 
-	getServices() {
-		this.log.info("enabled " + this.enabledServices.length + " services")
-		return this.enabledServices
-	}
 
 	createRxInput() {
 		// Create the RxInput object for later use.
